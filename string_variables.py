746f879de17d62392f1d49d119b7ffd497f7786a
blue-one94 <meem94osama@gmail.com>
2024-07-17 21:46:43 +0300

    Add files via upload

---
+++ string_variables.py
@@ -0,0 +1,15 @@
+# break = 5
+
+birth_year = 1994
+current_year = 2024
+
+age = current_year - birth_year
+
+print("Your age is ", age)
+
+first_name = 'Walter'
+middle_name ='John'
+last_name = 'Doe'
+
+print ('Your name is', first_name + ' ' + middle_name + ' ' + last_name) 
+
